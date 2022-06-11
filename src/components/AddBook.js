b3091f59571ce31a5ee57c84763021c33288f92b
brahimdidi <brahimgb1team@gmail.com>
2022-06-11 18:16:16 +0000

    remove commented code (required changes)

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,12 +5,8 @@ const AddBook = () => (
   <section className="addbook-section flex flex-column">
     <h1>Add new book</h1>
     <form className="form">
-      {/* <label id="title"> */}
       <input className="title" placeholder="title" name="title" />
-      {/* </label> */}
-      {/* <label id="author"> */}
       <input className="author" placeholder="author" name="author" />
-      {/* </label> */}
       <button type="submit">Add book</button>
     </form>
   </section>
