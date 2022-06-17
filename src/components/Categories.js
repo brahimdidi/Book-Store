80d13484a789566bbaa2af69956becf93a42be02
brahimdidi <brahimgb1team@gmail.com>
2022-06-17 19:16:22 +0000

    import and dispatch checkStatus action

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../store/categories/cateRedux';
 
-const Categories = () => (<button className="check-status" type="button">Check Status </button>);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleStatus = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <button className="check-status" type="button" onClick={() => handleStatus()}>Check Status </button>
+  );
+};
 
 export default Categories;
